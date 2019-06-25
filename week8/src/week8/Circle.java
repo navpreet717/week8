9b51f68bc96b9eae41a60b3aba5cb30801aca6de
748674 <748674@123-PC16.student.Tor.Loc>
2019-06-25 15:13:46 -0400

    FINAL CIRCLE ND MAIN

--- week8/src/week8/Circle.java
+++ week8/src/week8/Circle.java
@@ -11,28 +11,49 @@ public class Circle {
 			this.radius=radius;
 		}
 		
-		public static double area(double t1,double radius)
+		
+	
+		
+		
+		//custom methods
+		public double getArea()
 		{
-			
-			double r=t1*Math.pow(radius, 2);
-			return r;
-			
+			double area=Math.PI*this.radius*this.radius;
+			System.out.println("AreA is"+area);
+			return area;
+		}
+		
+		public double getCircumFerence()
+		{
+			double c=Math.PI*2*this.radius;
+			System.out.println("CircumFerence is"+c);
+			return c;
+		}
+		public double getDiameter()
+		{
+			double diameter=10;
+			System.out.println("Diameter is"+diameter);
+			return diameter;
+		}
+		//getter setter
+
+		public Double getRadius() {
+			return radius;
+		}
+
+
+		public void setRadius(Double radius) {
+			this.radius = radius;
 		}
 		
 		
-		
-		
-		
-		
-
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			
-			double d=area(5,2);
-			System.out.println("result is"+d);
 			
 
 		}
+		
 
 	}
 
--- week8/src/week8/Main.java
+++ week8/src/week8/Main.java
@@ -3,15 +3,82 @@ package week8;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		// TODO Auto-generated method stub
-		Circle c=new Circle((double) 5);
 		//Process for testing code
 		//1.read the requirements
 		//2decide what to test
-		   //r1 constructor sets radius
+		//r1 constructor sets radius
+		// actual :c.getRadius() //use if else for testing
+		Circle c=new Circle((double) 5);
+		double expectedRadius=5;
+		double actualRadius=c.getRadius();
+		if(expectedRadius==actualRadius)
+		{
+			
+			System.out.println("R1:WORKING");
+			System.out.println("Expected Result"+expectedRadius);
+			System.out.println("Actual Result"+actualRadius);
+		}
+		else
+		{
+			System.out.println("R1: not WORKING");
+		}
+	
+		
+		   
 		   //r2: areas calculates properly
+		
+		double expectedArea=Math.PI*5*5;
+		double actualArea=c.getArea();
+		if(expectedArea==actualArea)
+		{
+			
+			System.out.println("R2:WORKING");
+			System.out.println("Expected Area"+expectedArea);
+			System.out.println("Actual Area"+actualArea);
+		}
+		else
+		{
+			System.out.println("R2: not WORKING");
+		}
 		   //r3 circumference calculates properly
+		double expectedCircum=Math.PI*2*5;
+		double actuaCircum=c.getCircumFerence();
+		if(expectedCircum==actuaCircum)
+		{
+			
+			System.out.println("R3:WORKING");
+			System.out.println("Expected Circum"+expectedCircum);
+			System.out.println("Actual Circum"+actuaCircum);
+		}
+		else
+		{
+			System.out.println("R3: not WORKING");
+		}
 		   //r4  diameter calculates properly
+		double expectedD=2*5;
+		double actuaD=c.getDiameter();
+		if( expectedD==actuaD)
+		{
+			
+			System.out.println("R4:WORKING");
+			System.out.println("Expected d"+expectedD);
+			System.out.println("Actual d"+actuaD);
+		}
+		else
+		{
+			System.out.println("R4: not WORKING");
+		}
 		//3make test cases
 		//excepted result
 		//actual result
