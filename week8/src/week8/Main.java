5058996aaf6b6480b3eb52e86966ba18684e5382
748674 <748674@123-PC16.student.Tor.Loc>
2019-06-25 14:04:21 -0400

    initial main

---
+++ week8/src/week8/Main.java
@@ -0,0 +1,23 @@
+package week8;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c=new Circle((double) 5);
+		//Process for testing code
+		//1.read the requirements
+		//2decide what to test
+		   //r1 constructor sets radius
+		   //r2: areas calculates properly
+		   //r3 circumference calculates properly
+		   //r4  diameter calculates properly
+		//3make test cases
+		//excepted result
+		//actual result
+		//4
+		//5 debug
+
+	}
+
+}
